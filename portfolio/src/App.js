e446a705ae49c52c38dec8e451440bee597311d1
Andrew Bjork <bjorkandrew1@gmail.com>
2020-05-09 17:27:17 -0500

    update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+// import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+// constructor(props) {
+//   super(props);
+//   // Manage the sort state in your application
+//   this.state = {
+//     sortName: undefined,
+//     sortOrder: undefined,
+//     // error: null,
+//     isLoaded: false,
+//     apiResult: [],
+//     apiFirstName: [],
+//   };
+//   //this.onSortChange = this.onSortChange.bind(this);
+// }
+
+
+
+
+
+//   fetch("https://api.spotify.com/v1/audio-analysis/79b94fa7ac944da6be26a6f34b562afc", {
+//   method: "GET",
+//   headers: {
+//     Authorization: `Bearer ${userAccessToken}`     
+//   }
+// })
+// .then(response => response.json())
+// .then(({beats})) => {
+//   beats.forEach((beat, index) => {
+//     console.log(`Beat ${index} starts at ${beat.start}`);
+//   })
+// }
+
+// const url = "https://api.spotify.com/v1/audio-analysis/6EJiVf7U0p1BBfs0qqeb1f";
+// const response = fetch(url);
+// const data = response;
+// const config = {
+//   headers: {'{79b94fa7ac944da6be26a6f34b562afc}': 'http://localhost:3000/'}
+// };
+
+// console.log(data, config);
+
+  // fetch("http://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6")
+  //   .then((res) => res.json())
+  //   .then((result) => {
+  //     this.setState({
+  //       isLoaded: true,
+  //       apiResult: result.results,
+  //     });
+  //   });
+
+  // this.setState({
+  //   apiFirstName: this.state.apiResult.name,
+  // });
+
+
+
+
+  render() {
+
+    return(
+  <Wrapper>
+    <Title>
+
+    </Title>
+
+
+  </Wrapper>
+    )
+  }
 }
+
+
 
 export default App;
---
+++ portfolio/src/components/Title/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import "./style.css";
+
+
+ return(
+
+<div>Hello</div>
+   
+ )
+
+}
+
+export default Title;
