f1f13a50fc94d1a101b0ba124b121c56d86df0b9
Andrew Bjork <bjorkandrew1@gmail.com>
2020-08-18 08:38:34 -0500

    color, style update

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -10,16 +10,16 @@ function Title() {
 <div className = "hello">
 
 
-<div className="back collection with-header">
-<div class="collection-header"><h4>Projects</h4></div>
-<Link to="/audio" class = "collection-item color">Audio Mind</Link>
-<Link to="/skills" class = "collection-item color">Sllik Skills</Link>
-<Link to="/notes" class = "collection-item color">Notetaker Tasks</Link>
-<Link to="/employee" class = "collection-item color">Employee Table</Link>
-<Link to="/covid" class = "collection-item color">Covid-19 Tool</Link>
-<Link to="/quiz" class = "collection-item color">Capitals Quiz App</Link>
-<Link to="/weather" class = "collection-item color">Weather Forecast</Link>
-<Link to="/" class = "collection-item color">Home</Link>
+<div className="back collection with-header" >
+<div class="collection-header" style = {{backgroundColor: '#3E78B2', color :'#E0E0E2'}}><h4>Projects</h4></div>
+<Link to="/audio" class = "collection-item color" style = {{color: '#0B6E4F'}}>Audio Mind</Link>
+<Link to="/skills" class = "collection-item color" style = {{color: '#0B6E4F'}}>Sllik Skills</Link>
+<Link to="/notes" class = "collection-item color" style = {{color: '#0B6E4F'}}>Notetaker Tasks</Link>
+<Link to="/employee" class = "collection-item color" style = {{color: '#0B6E4F'}}>Employee Table</Link>
+<Link to="/covid" class = "collection-item color" style = {{color: '#0B6E4F'}}>Covid-19 Tool</Link>
+<Link to="/quiz" class = "collection-item color" style = {{color: '#0B6E4F'}}>Capitals Quiz App</Link>
+<Link to="/weather" class = "collection-item color" style = {{color: '#0B6E4F'}}>Weather Forecast</Link>
+
 
 
 </div>
--- src/components/pages/Info.js
+++ src/components/pages/Info.js
@@ -1,16 +1,16 @@
 import React from "react";
-import link from "./linkpic.jpg";
+import link from "./andrew.jpg";
 
 function Info() {
   return (
 <div >
     
  <div className= "row">
- <div className = "col s1"></div>
- <div className = "col s4">
+ 
+ <div className = "col s5">
 
 
- <table className = "striped">
+ <table className = "striped" style = {{padding: '15px', border : '1px solid #E0E0E2', marginLeft: '50px'}}>
         <thead>
           <tr>
               <th>Contact</th>
@@ -35,14 +35,27 @@ function Info() {
           </tr>
           <tr>
             <td>Github</td>
-            <td><a class="waves-effect waves-light btn">Follow</a></td>
+            <td><a href="https://github.com/bjork1" target = "_blank" class="waves-effect waves-light btn" style = {{backgroundColor: '#3E78B2'}}>Follow</a></td>
             <td></td>
           </tr>
         </tbody>
       </table>
 
 
-
+   
+  
+      <div class="card " style = {{width: '400px', marginTop: '50px', backgroundColor: '#FA9F42'}}>
+        <div class="card-content white-text">
+          <span class="card-title">Summary</span>
+          <p>Agile business professional with specialties in troubleshooting, marketing, and web development. Holds certifications in Full Stack Web Development and ScrumMaster, and a Bachelor’s degree in Business Administration. </p>
+        </div>
+        {/* <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div> */}
+      </div>
+ 
+  
        
 
 
@@ -50,13 +63,13 @@ function Info() {
   
         </div>
         <div className = "col s3"></div>
-        <div className = "col s4">
+        <div className = "col s4" >
         
-      <div class="card">
+      <div class="card" style = {{textAlign: 'center', left:'50%'}}>
         <div class="card-image">
           <img src={link}/>
        
-          <a href="https://www.linkedin.com/in/bjorkandrew/" target = "_blank" class="btn-floating halfway-fab waves-effect waves-light red btn-floating btn-large pulse"><i class="material-icons">add</i></a>
+          <a href="https://www.linkedin.com/in/bjorkandrew/" target = "_blank" class="btn-floating halfway-fab waves-effect waves-light btn-floating btn-large pulse" style = {{backgroundColor: '#3E78B2'}}><i class="material-icons">add</i></a>
         </div>
         <div class="card-content">
           <p style = {{marginTop: '20px'}}>Let's connect on LinkedIn.</p>
@@ -64,6 +77,37 @@ function Info() {
     </div>
   </div>
 
+  <table className = "centered">
+        <thead>
+          <tr>
+              <th></th>
+              <th>Web</th>
+              <th>Dev</th>
+              <th>Skills</th>
+              <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>ReactJS</td>
+            <td>jQuery</td>
+            <td>Node</td>
+            <td>Materialize</td>
+            <td>AWS</td>
+          </tr>
+          <tr>
+            <td>Express</td>
+            <td>MongoDB</td>
+            <td>MySQL</td>
+            <td>Bootstrap</td>
+            <td>NextJS</td>
+          </tr>
+          
+        </tbody>
+      </table>
+    
+  
 
         </div>
         
